350753b557e41f19545a93a312c087efad776aeb
Fatih <151558909+fatihalkn@users.noreply.github.com>
2024-06-19 01:47:18 +0300

    fix API key

--- Math-AI/Math-AI/NetworkLayer/Core/Constants/NetworkConstants.swift
+++ Math-AI/Math-AI/NetworkLayer/Core/Constants/NetworkConstants.swift
@@ -9,6 +9,6 @@ import Foundation
 
 enum NetworkConstants {
     static let chatBaseUrl = "https://api.openai.com/v1/chat/completions"
-    static var chatAPIKey = "sk-proj-34jHTWxtHQu6t9zYUL3yT3BlbkFJCGwYmvNXat2eXaDCiP2w"
+    static var chatAPIKey = ""
 }
  
